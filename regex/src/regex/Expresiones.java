35b0a194c4a5f90f74ae08623a6ab2deb5ad1c90
SalvaCM <Salvador.CascadoMa@Elorrieta-Errekamari.com>
2019-12-10 09:37:59 +0100

    Estructura regex

---
+++ regex/src/regex/Expresiones.java
@@ -0,0 +1,15 @@
+package regex;
+
+import java.util.ArrayList;
+
+public class Expresiones {
+       ArrayList <String> expresiones= new ArrayList<String>();
+       
+       public Expresiones() {
+    	   expresiones.add("/num");
+    	   expresiones.add("/!num");
+    	   expresiones.add("/!*");
+    	   expresiones.add("/tam");
+       }
+       
+}
---
+++ regex/src/regex/Regex.java
@@ -0,0 +1,32 @@
+package regex;
+
+public class Regex {
+	Expresiones expresiones = new Expresiones();
+	public Regex() {
+		
+	}
+	public boolean validarPatron(String expresion) {
+		
+		if ()
+		{
+			return true;
+		}
+		else
+		{
+			System.out.println("Expresion mal construida");
+			return false;
+		}
+	}
+	public boolean validarCadena(String expresion,String cadena) {
+		
+		if ()
+		{
+			return true;
+		}
+		else
+		{
+			System.out.println("Cadena no pasa validacion");
+			return false;
+		}
+	}
+}
