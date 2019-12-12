8c9e5dcf0007ebee3332ef1442553fb7d3a73c2e
IN1DM3B_09 <IN1DM3B_09@H015112.NET>
2019-12-12 13:19:47 +0100

    Crear metodos de validacion de expresion

--- regex/src/regex/Expresiones.java
+++ regex/src/regex/Expresiones.java
@@ -3,13 +3,57 @@ package regex;
 import java.util.ArrayList;
 
 public class Expresiones {
-       ArrayList <String> expresiones= new ArrayList<String>();
-       
-       public Expresiones() {
-    	   expresiones.add("/num");
-    	   expresiones.add("/!num");
-    	   expresiones.add("/!*");
-    	   expresiones.add("/tam");
-       }
-       
+
+	private ArrayList<String> expresiones = new ArrayList<String>();
+	private static final String EXPRESION_SEPARATOR = ",";
+
+	public Expresiones() {
+		expresiones.add("/d");
+		expresiones.add("/!d");
+		expresiones.add("/c");
+		expresiones.add("/!c");
+		expresiones.add("/*");
+		expresiones.add("/!*");
+	}
+
+	public boolean validarRegex(String regex) {
+		String[] regexParts = regex.split(EXPRESION_SEPARATOR);
+		for (String part : regexParts) {
+			if (!evaluarRegex(part)) {
+				System.out.println("Mal construido");
+				return false;
+			}
+		}
+		System.out.println("Bien construido");
+		return true;
+	}
+
+	private boolean evaluarRegex(String regex) {
+		for (String exp : expresiones) {
+			if (regex.startsWith(exp)) 
+				if (regex.length() != exp.length()) {
+					if (evaluarRango(regex.substring(exp.length())))
+						return true;
+				}else
+					return true;
+		}
+		return false;
+	}
+
+	private boolean evaluarRango(String rango) {
+		if (rango.startsWith("[") && rango.endsWith("]")) {
+			String substring = rango.substring(1, rango.length() - 1);
+			String[] numeros = substring.split("-");
+			for (String num : numeros) {
+				try {
+					Integer.parseInt(num);
+				} catch (NumberFormatException ex) {
+					return false;
+				}
+			}
+			return true;
+		} else
+			return false;
+	}
+
 }
--- regex/src/regex/Principal.java
+++ regex/src/regex/Principal.java
@@ -3,8 +3,12 @@ package regex;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Expresiones expresiones = new Expresiones();
+		String expresionOK = "/d[3]";
+		String expresionNotOK = "/d[3-2],/!d[5],/!c";
+		String cadena = "nfkjdaso";
+		expresiones.validarRegex(expresionOK);
+		expresiones.validarRegex(expresionNotOK);
 	}
 
 }
